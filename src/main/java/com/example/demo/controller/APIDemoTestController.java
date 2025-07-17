666eead86c04971255bde8a294d79f62c6d6bc88
Chlorine001 <1151259938@qq.com>
2025-07-17 18:24:00 +0800

    feat(controller): 添加 APIDemoTestController 实现简单 API 接口

---
+++ src/main/java/com/example/demo/controller/APIDemoTestController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @Author chenglong
+ * @className APIDemoTestController
+ * @Description 一个简单api接口
+ * @Date 2025-07-17
+ */
+
+@RestController
+public class APIDemoTestController {
+    @GetMapping("/get")
+    public String get() {
+        return "get";
+    }
+
+    @PostMapping("/post")
+    public String post() {
+        return "post";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
