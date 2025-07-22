60fd03bf92a7873b705118ce6e1ea8468fe3c1f0
Chlorine001 <1151259938@qq.com>
2025-07-22 18:10:33 +0800

    feat(security): 添加 SecurityConfig 类
    - 新增 SecurityConfig 类，继承自 WebSecurityConfigurerAdapter
    - 用于配置 Spring Security 相关的安全设置
    
    Signed-off-by: Chlorine001 <1151259938@qq.com>

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @version 1.0
+ * @Author chenglong
+ * @className SecurityConfig
+ * @Description
+ * @Date 2025-07-22
+ */
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
