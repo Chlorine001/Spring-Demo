214533b3d8506d2ddf74dd8d9aae74804fd6769c
Chlorine001 <1151259938@qq.com>
2025-08-01 17:19:33 +0800

    test: 更新 MybatisPlusApplicationTests 测试类
    - 将 User 实体类替换为 SysUsers 实体类
    - 将 UserMapper 替换为 SysUsersMapper
    - 更新相关测试方法，适应新的实体类和 mapper
    - 调整属性名称，如 number改为 age，description 改为 email
    
    Signed-off-by: Chlorine001 <1151259938@qq.com>

--- src/test/java/com/example/demo/MybatisPlusApplicationTests.java
+++ src/test/java/com/example/demo/MybatisPlusApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.entity.User;
-import com.example.demo.mapper.UserMapper;
+import com.example.demo.entity.SysUsers;
+import com.example.demo.mapper.SysUsersMapper;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,21 +22,21 @@ class MybatisPlusApplicationTests {
     //我们也可以编写自己的扩展方法
     //通过类型自动装配UserMapper接口
     @Autowired
-    private UserMapper userMapper;
+    private SysUsersMapper sysUsersMapper;
 
     //测试插入用户数据
     @Test
     public void testInsert() {
         //获取User对象
-        User user = new User();
+        SysUsers user = new SysUsers();
         //设置用户名和年龄以及邮箱等属性
         //使用ASSign_UUID主键策略
         user.setName("杨宗纬");
-        user.setNumber("44");
-        user.setDescription("yzw123456@qq.com");
+        user.setAge(44);
+        user.setEmail("yzw123456@qq.com");
 
         //返回受影响的行数(自动帮我们生成Id)
-        int result = userMapper.insert(user);
+        int result = sysUsersMapper.insert(user);
         //打印受影响行数
         System.out.println(result);
         //打印插入的用户信息
@@ -46,16 +46,16 @@ class MybatisPlusApplicationTests {
     @Test
     public void testUpdate() {
         //获取User对象
-        User user = new User();
+        SysUsers user = new SysUsers();
         //设置修改用户的Id
         user.setId(1L);
         //设置用户要修改的信息(修改多个属性)
         user.setName("周杰伦");
-        user.setNumber("26");
-        user.setDescription("zjl123456@qq.com");
+        user.setAge(27);
+        user.setEmail("zjl123456@qq.com");
         //通过Id修改用户,并且返回影响行数
         //注意：虽然updateById是通过Id修改信息，但是参数其实是一个对象！
-        int result = userMapper.updateById(user);
+        int result = sysUsersMapper.updateById(user);
         //打印受影响行数
         System.out.println(result);
     }
